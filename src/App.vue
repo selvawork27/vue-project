62f6e1a317785e12b43654f4d2a591237ae86709
selvawork27 <selvad274@gmail.com>
2026-01-28 18:58:21 +0530

    component  update

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
 import {ref,getCurrentInstance} from "vue"
 const message=ref("hello world")
 
-const globals=getCurrentInstance().appContext.config.globalProperties
-console.log(globals.$appName)
+// const globals=getCurrentInstance().appContext.config.globalProperties
+// console.log(globals.$appName)
 </script>
 
 <template>
   <h1>You did it! {{$appName}}</h1>
+  <p>Today Date : {{$formatDate}}</p>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
+  </p>
+  <p>
+    <Homes/>
   </p>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Home from './Home.vue'
 const app=createApp(App)
+app.component("Homes",Home)
 app.config.globalProperties.$appName="projectName"
 app.config.globalProperties.$formatDate=(date)=>{
     return new Date(date).toLocaleDateString()
